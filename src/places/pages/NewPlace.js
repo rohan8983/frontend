462558b0ecf4e2481677e08f09c404739ff035b3
Rohan Chougule <34051413+rohan8983@users.noreply.github.com>
2020-05-06 11:40:12 +0530

    changes for add new place form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/utils/validators";
 import "./NewPlace.css";
-
+let count = 0;
 const NewPlace = () => {
-  return <form className="place-form"><Input element="input" type="text" label="Title" /></form>;
+  const titleInputHandler = useCallback((id, value, isValid) => {
+    console.log("count" + count++);
+  }, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+    console.log("count" + count++);
+  }, []);
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+        errorText="Please Enter valid text."
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        onInput={descriptionInputHandler}
+        errorText="Please Enter valid description (at least 5 characters)."
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
